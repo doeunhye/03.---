432103dc64419ac9ac4e288d9b6b46502bcbf536
doeunhye <ehdmsgp123@naver.com>
2018-10-02 15:49:33 +0900

    연산 초분

--- 연산.c
+++ 연산.c
@@ -4,20 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[])
- {int op1, op2;
- int sum, sub;
- 
- printf("input integers :");
- scanf("%i %i", &op1, &op2);
- 
- sum = op1 + op2;
- sub = op1 - op2;
- 
- printf("+ operation : %i/n", sum);
- printf("- operation : %i/n", sub);
- printf("* operation : %i/n", op1*op2);
- printf("/ operation : %i/n", op1/op2);
- printf("%% operation : %i/n", op1%op2);
- 
+ {int sec;
+  
+  printf("input : ");
+  scanf("%d" , &sec);
+  
+  printf("%d sec, is %i : %i/n", sec, sec/60, sec%60);
+
 	return 0;
 }
